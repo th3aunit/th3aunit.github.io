e97ebf92218d09890646510a5149de536329b25e
Amalthea <amalthea@faen.dev>
2024-06-18 22:34:02 +0000

    Generate size queries

--- scripts/generateIndex.sh
+++ scripts/generateIndex.sh
@@ -2,17 +2,26 @@
 
 # function to generate index.html for the given directory
 generateIndex() {
+    echo "" > "$1/index.html" # clear out any existing indexes
     for file in "$1"/*; do
         if [ "${file##*/}" = "index.html" ]; then
             continue
         fi
         if [ -d "$file" ]; then
-            echo "${file##*/}/"
+            echo "${file##*/}/" >> "$1/index.html"
         else
-            echo "${file##*/}"
+            echo "Processing ${file##*}"
+            echo "${file##*/}" >> "$1/index.html"
+            generateSizeIndex "$1" "${file##*/}"
         fi
     done
 }
 
+# function to generate index.html?<filename>
+# the file contains just the size of the file in bytes
+generateSizeIndex() {
+    stat -c%s "$1/$2" > "$1/index.html?$2"
+}
+
 # generate index.html for personas
-generateIndex personas > personas/index.html
+generateIndex personas
