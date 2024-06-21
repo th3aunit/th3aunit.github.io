687280f1d210691ee1c4bf3d58106ea21628f351
Amalthea <amalthea@faen.dev>
2024-06-21 19:52:12 +0000

    Move scripts to a dedicated scritps folder

---
+++ scripts/index.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: text/plain');
+
+if(!isset($queryString))
+    $queryString = @$_SERVER['QUERY_STRING'];
+
+if($queryString != '') {
+    $queryString = str_replace('/', '_', $queryString);
+    echo(filesize($queryString));
+    return;
+}
+
+function filter_filenames(string $file) {
+    $unlistedFiles = [
+        '.htaccess',
+        'index.php',
+        'index.html',
+        '..',
+        '.'
+    ];
+    return array_search($file, $unlistedFiles) === false;
+}
+
+$items = scandir(getcwd());
+$filteredItems = array_filter($items, 'filter_filenames');
+echo implode("\n", $filteredItems);
