3f248627257568be7ad1913ac2e17516be73ec2f
Amalthea <amalthea@faen.dev>
2024-06-24 12:40:37 +0200

    Update indexes to use code written with Claude

--- db/index.php
+++ db/index.php
@@ -1,26 +1,64 @@
 <?php
+
+declare(strict_types=1);
+
+namespace FileListingScript;
+
+/**
+ * List of files and directories to exclude from the listing
+ */
+const EXCLUDE_LIST = [
+    '.',
+    '..',
+    '.htaccess',
+    'index.php',
+    'index.html',
+];
+
 header('Content-Type: text/plain');
 
-if(!isset($queryString))
-    $queryString = @$_SERVER['QUERY_STRING'];
+$queryString = $_SERVER['QUERY_STRING'] ?? '';
 
-if($queryString != '') {
-    $queryString = str_replace('/', '_', $queryString);
-    echo(filesize($queryString));
-    return;
+if ($queryString !== '') {
+    handleFileSize($queryString);
+} else {
+    listFiles();
 }
 
-function filter_filenames(string $file) {
-    $unlistedFiles = [
-        '.htaccess',
-        'index.php',
-        'index.html',
-        '..',
-        '.'
-    ];
-    return array_search($file, $unlistedFiles) === false;
+/**
+ * Handle file size request
+ *
+ * @param string $queryString
+ * @return void
+ */
+function handleFileSize($queryString)
+{
+    $filename = htmlspecialchars($queryString, ENT_QUOTES, 'UTF-8');
+    $filename = basename($filename);
+    $filePath = __DIR__ . DIRECTORY_SEPARATOR . $filename;
+    
+    if (is_file($filePath)) {
+        echo filesize($filePath);
+    } else {
+        echo "File not found or is not accessible.";
+    }
 }
 
-$items = scandir(getcwd());
-$filteredItems = array_filter($items, 'filter_filenames');
-echo implode("\n", $filteredItems);
+/**
+ * List files in the current directory
+ *
+ * @return void
+ */
+function listFiles()
+{
+    $items = scandir(__DIR__) ?: [];
+    $filteredItems = array_filter(
+        $items,
+        function ($item) {
+            return !in_array($item, EXCLUDE_LIST, true)
+                && is_file(__DIR__ . DIRECTORY_SEPARATOR . $item);
+        }
+    );
+
+    echo implode("\n", $filteredItems);
+}
